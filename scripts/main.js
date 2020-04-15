9b1b72ed755299ee0e353ff05d439f3ec15b041c
Himanshu Raghav <itsmeraghuvanshi@gmail.com>
2020-04-15 20:14:51 +0530

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -8,23 +8,3 @@ myImage.onclick = function () {
     }
 
 }
-
-let myButton = document.querySelector('button');
-let myHeading = document.querySelector('h1');
-
-function setUserName() {
-    let myName = prompt('Please enter your name.');
-    localStorage.setItem('name', myName);
-    myHeading.textContent = 'New York is cool, ' + myName;
-}
-
-if(!localStorage.getItem('name')) {
-    setUserName();
-} else {
-    let storedName = localStorage.getItem('name');
-    myHeading.textContent = 'New York is cool, ' + storedName;
-}
-
-myButton.onclick = function() {
-    setUserName();
-}
